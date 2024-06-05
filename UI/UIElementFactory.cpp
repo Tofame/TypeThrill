8774f421b5d91438106416b0d90a9ead8d3e4103
Tofame <niewiadoma.65@gmail.com>
2024-06-05 13:22:42 +0200

    Make UIElementFactory.cpp file
    Move method definitions out of header file.

---
+++ UI/UIElementFactory.cpp
@@ -0,0 +1,173 @@
+#include "UIElementFactory.h"
+
+sf::Color UIElementFactory::ColorBlue = sf::Color(52, 194, 199);
+sf::Color UIElementFactory::ColorGreen = sf::Color(52, 235, 88);
+sf::Color UIElementFactory::ColorGray = sf::Color(105,104,102);
+sf::Color UIElementFactory::ColorGrayDarkLO = sf::Color(64,64,64, 120);
+sf::Color UIElementFactory::ColorGrayDark = sf::Color(54, 54, 54);
+
+ComboBox* UIElementFactory::createComboBox(const std::string& fieldValue, sf::Vector2f posRatios, const std::string& textValue) {
+    sf::Vector2f size = {200, 30};
+    auto element = new ComboBox(size, posRatios);
+
+    auto& rect = element->body;
+    rect.setFillColor(UIElementFactory::ColorGrayDark);
+    rect.setOutlineColor(sf::Color::White);
+    rect.setOutlineThickness(3);
+
+    if(textValue.empty() == false) {
+        auto text = sf::Text();
+        text.setFont(FontManager::Fonts["times"]);
+        text.setCharacterSize(GameInterface::smallCharacterSize);
+
+        text.setFillColor(sf::Color::White);
+        text.setOutlineColor(sf::Color::Black);
+        text.setOutlineThickness(1);
+
+        text.setString(textValue);
+
+        element->setText(text);
+    }
+
+    if(fieldValue.empty() == false) {
+        auto chosenText = sf::Text();
+        chosenText.setFont(FontManager::Fonts["aleoItalic"]);
+        chosenText.setCharacterSize(GameInterface::smallCharacterSize);
+        chosenText.setFillColor(sf::Color::White);
+        chosenText.setString(fieldValue);
+
+        element->setChosenText(chosenText);
+    }
+
+    return element;
+}
+
+DynamicTextLabel* UIElementFactory::createStatisticsDynamicLabel(sf::Vector2f ratios, std::function<std::string()> const& onUpdateString) {
+    auto element = new DynamicTextLabel(ratios, onUpdateString);
+
+    element->getText().setCharacterSize(GameInterface::mediumLiteCharacterSize);
+    element->setAlignType(ALIGN_NONE);
+
+    return element;
+}
+
+DynamicTextLabel* UIElementFactory::createInfoDynamicLabel(sf::Vector2f ratios, std::function<std::string()> const& onUpdateString) {
+    auto element = new DynamicTextLabel(ratios, onUpdateString);
+
+    element->getText().setCharacterSize(GameInterface::smallCharacterSize);
+    element->setAlignType(ALIGN_NONE);
+
+    return element;
+}
+
+Checkbox* UIElementFactory::createMenuCheckbox(float sizeMultiplier, sf::Vector2f posRatios, std::string textValue, bool defaultEnabled, std::function<void(bool value)> const& onCheckboxUpdate) {
+    auto element = new Checkbox(sizeMultiplier, posRatios, defaultEnabled);
+
+    element->onCheckboxUpdate = onCheckboxUpdate;
+
+    if(textValue.empty() == false) {
+        auto text = sf::Text();
+        text.setFont(FontManager::Fonts["times"]);
+        text.setCharacterSize(GameInterface::smallCharacterSize);
+
+        text.setFillColor(sf::Color::White);
+        text.setOutlineColor(sf::Color::Black);
+        text.setOutlineThickness(1);
+
+        text.setString(textValue);
+
+        element->setText(text);
+    }
+
+    return element;
+}
+
+TextField* UIElementFactory::createTextField(const std::string& fieldValue, sf::Vector2f posRatios, std::string textValue, std::wstring pattern) {
+    sf::Vector2f size = {200, 30};
+    auto element = new TextField(size, posRatios);
+
+    // Set allowed input - a regex pattern (e.g. only DIGITS (1-9) allowed)
+    element->setPattern(pattern);
+
+    auto& rect = element->body;
+    rect.setFillColor(UIElementFactory::ColorGray);
+    rect.setOutlineColor(sf::Color::White);
+    rect.setOutlineThickness(3);
+
+    if(textValue.empty() == false) {
+        auto text = sf::Text();
+        text.setFont(FontManager::Fonts["times"]);
+        text.setCharacterSize(GameInterface::smallCharacterSize);
+
+        text.setFillColor(sf::Color::White);
+        text.setOutlineColor(sf::Color::Black);
+        text.setOutlineThickness(1);
+
+        text.setString(textValue);
+
+        element->setText(text);
+    }
+
+    auto input = sf::Text();
+    input.setFont(FontManager::Fonts["aleoItalic"]);
+    input.setCharacterSize(GameInterface::smallCharacterSize);
+    input.setFillColor(sf::Color::White);
+    input.setString(fieldValue);
+
+    // I-line, Cursour etc. <- Its the line that blinks and indicates where your next letter will be
+    element->pointLine.setFont(FontManager::Fonts["times"]);
+    element->pointLine.setCharacterSize(GameInterface::smallCharacterSize);
+    element->pointLine.setFillColor(sf::Color::White);
+    element->pointLine.setString("|");
+
+    element->setInput(input);
+
+    return element;
+}
+
+TextField* UIElementFactory::createTextField(const std::string& fieldValue, sf::Vector2f posRatios, std::string textValue, std::wstring pattern, std::function<void()> const& onTextFieldUpdate) {
+    auto element = UIElementFactory::createTextField(fieldValue, posRatios, textValue, pattern);
+    element->onTextFieldUpdate = onTextFieldUpdate;
+
+    return element;
+}
+
+Button* UIElementFactory::createMenuButton(const std::string& name, const std::function<void()>& onClick, sf::Vector2f posRatios) {
+    sf::Vector2f size = {200, 50};
+    return createMenuButton(name, onClick, posRatios, size);
+}
+
+Button* UIElementFactory::createMenuButton(const std::string& name, const std::function<void()>& onClick, sf::Vector2f posRatios, sf::Vector2f size) {
+    auto element = new Button(size, posRatios, onClick);
+
+    auto& rect = element->body;
+    rect.setFillColor(UIElementFactory::ColorBlue);
+    rect.setOutlineColor(sf::Color::White);
+    rect.setOutlineThickness(4);
+
+    auto text = sf::Text();
+    text.setFont(FontManager::Fonts["jaro"]);
+    text.setCharacterSize(GameInterface::smallCharacterSize);
+    text.setFillColor(sf::Color::White);
+    text.setOutlineColor(sf::Color::Black);
+    text.setOutlineThickness(1);
+    text.setString(name);
+    text.setPosition(rect.getPosition());
+    text.move(rect.getSize().x/2 - text.getGlobalBounds().width/2, rect.getSize().y/2 - text.getGlobalBounds().height + text.getGlobalBounds().height/3);
+    element->setText(text);
+
+    return element;
+}
+
+Panel* UIElementFactory::createPanel(UIElement* parent, sf::Vector2f size, sf::Vector2f posRatios, PanelType panelType) {
+    auto panel = new Panel(parent, size, posRatios);
+
+    panel->setPanelType(panelType);
+
+    auto& rect = panel->body;
+    rect.setFillColor(sf::Color::Transparent);
+    rect.setOutlineColor(sf::Color::Green);
+    rect.setOutlineThickness(3);
+
+    return panel;
+}
--- UI/UIElementFactory.h
+++ UI/UIElementFactory.h
@@ -13,175 +13,30 @@
 
 class UIElementFactory {
 public:
-    inline static auto ColorBlue = sf::Color(52, 194, 199);
-    inline static auto ColorGreen = sf::Color(52, 235, 88);
-    inline static auto ColorGray = sf::Color(105,104,102);
-    inline static auto ColorGrayDarkLO = sf::Color(64,64,64, 120);
-    inline static auto ColorGrayDark = sf::Color(54, 54, 54);
+    static sf::Color ColorBlue;
+    static sf::Color ColorGreen;
+    static sf::Color ColorGray;
+    static sf::Color ColorGrayDarkLO;
+    static sf::Color ColorGrayDark;
 
-    static ComboBox* createComboBox(const std::string& fieldValue, sf::Vector2f posRatios, const std::string& textValue) {
-        sf::Vector2f size = {200, 30};
-        auto element = new ComboBox(size, posRatios);
+    static ComboBox* createComboBox(const std::string& fieldValue, sf::Vector2f posRatios, const std::string& textValue);
 
-        auto& rect = element->body;
-        rect.setFillColor(ColorGrayDark);
-        rect.setOutlineColor(sf::Color::White);
-        rect.setOutlineThickness(3);
+    static DynamicTextLabel* createStatisticsDynamicLabel(sf::Vector2f ratios, std::function<std::string()> const& onUpdateString);
 
-        if(textValue.empty() == false) {
-            auto text = sf::Text();
-            text.setFont(FontManager::Fonts["times"]);
-            text.setCharacterSize(GameInterface::smallCharacterSize);
+    static DynamicTextLabel* createInfoDynamicLabel(sf::Vector2f ratios, std::function<std::string()> const& onUpdateString);
 
-            text.setFillColor(sf::Color::White);
-            text.setOutlineColor(sf::Color::Black);
-            text.setOutlineThickness(1);
+    static Checkbox* createMenuCheckbox(float sizeMultiplier, sf::Vector2f posRatios,
+        std::string textValue, bool defaultEnabled, std::function<void(bool value)> const& onCheckboxUpdate);
 
-            text.setString(textValue);
+    static TextField* createTextField(const std::string& fieldValue, sf::Vector2f posRatios,
+        std::string textValue, std::wstring pattern);
 
-            element->setText(text);
-        }
+    static TextField* createTextField(const std::string& fieldValue, sf::Vector2f posRatios,
+        std::string textValue, std::wstring pattern, std::function<void()> const& onTextFieldUpdate);
 
-        if(fieldValue.empty() == false) {
-            auto chosenText = sf::Text();
-            chosenText.setFont(FontManager::Fonts["aleoItalic"]);
-            chosenText.setCharacterSize(GameInterface::smallCharacterSize);
-            chosenText.setFillColor(sf::Color::White);
-            chosenText.setString(fieldValue);
+    static Button* createMenuButton(const std::string& name, const std::function<void()>& onClick, sf::Vector2f posRatios);
 
-            element->setChosenText(chosenText);
-        }
+    static Button* createMenuButton(const std::string& name, const std::function<void()>& onClick, sf::Vector2f posRatios, sf::Vector2f size);
 
-        return element;
-    }
-
-    static DynamicTextLabel* createStatisticsDynamicLabel(sf::Vector2f ratios, std::function<std::string()> const& onUpdateString) {
-        auto element = new DynamicTextLabel(ratios, onUpdateString);
-
-        element->getText().setCharacterSize(GameInterface::mediumLiteCharacterSize);
-        element->setAlignType(ALIGN_NONE);
-
-        return element;
-    }
-
-    static DynamicTextLabel* createInfoDynamicLabel(sf::Vector2f ratios, std::function<std::string()> const& onUpdateString) {
-        auto element = new DynamicTextLabel(ratios, onUpdateString);
-
-        element->getText().setCharacterSize(GameInterface::smallCharacterSize);
-        element->setAlignType(ALIGN_NONE);
-
-        return element;
-    }
-
-    static Checkbox* createMenuCheckbox(float sizeMultiplier, sf::Vector2f posRatios, std::string textValue, bool defaultEnabled, std::function<void(bool value)> const& onCheckboxUpdate) {
-        auto element = new Checkbox(sizeMultiplier, posRatios, defaultEnabled);
-
-        element->onCheckboxUpdate = onCheckboxUpdate;
-
-        if(textValue.empty() == false) {
-            auto text = sf::Text();
-            text.setFont(FontManager::Fonts["times"]);
-            text.setCharacterSize(GameInterface::smallCharacterSize);
-
-            text.setFillColor(sf::Color::White);
-            text.setOutlineColor(sf::Color::Black);
-            text.setOutlineThickness(1);
-
-            text.setString(textValue);
-
-            element->setText(text);
-        }
-
-        return element;
-    }
-
-    static TextField* createTextField(const std::string& fieldValue, sf::Vector2f posRatios, std::string textValue, std::wstring pattern) {
-        sf::Vector2f size = {200, 30};
-        auto element = new TextField(size, posRatios);
-
-        // Set allowed input - a regex pattern (e.g. only DIGITS (1-9) allowed)
-        element->setPattern(pattern);
-
-        auto& rect = element->body;
-        rect.setFillColor(ColorGray);
-        rect.setOutlineColor(sf::Color::White);
-        rect.setOutlineThickness(3);
-
-        if(textValue.empty() == false) {
-            auto text = sf::Text();
-            text.setFont(FontManager::Fonts["times"]);
-            text.setCharacterSize(GameInterface::smallCharacterSize);
-
-            text.setFillColor(sf::Color::White);
-            text.setOutlineColor(sf::Color::Black);
-            text.setOutlineThickness(1);
-
-            text.setString(textValue);
-
-            element->setText(text);
-        }
-
-        auto input = sf::Text();
-        input.setFont(FontManager::Fonts["aleoItalic"]);
-        input.setCharacterSize(GameInterface::smallCharacterSize);
-        input.setFillColor(sf::Color::White);
-        input.setString(fieldValue);
-
-        // I-line, Cursour etc. <- Its the line that blinks and indicates where your next letter will be
-        element->pointLine.setFont(FontManager::Fonts["times"]);
-        element->pointLine.setCharacterSize(GameInterface::smallCharacterSize);
-        element->pointLine.setFillColor(sf::Color::White);
-        element->pointLine.setString("|");
-
-        element->setInput(input);
-
-        return element;
-    }
-
-    static TextField* createTextField(const std::string& fieldValue, sf::Vector2f posRatios, std::string textValue, std::wstring pattern, std::function<void()> const& onTextFieldUpdate) {
-        auto element = UIElementFactory::createTextField(fieldValue, posRatios, textValue, pattern);
-        element->onTextFieldUpdate = onTextFieldUpdate;
-
-        return element;
-    }
-
-    static Button* createMenuButton(const std::string& name, const std::function<void()>& onClick, sf::Vector2f posRatios) {
-        sf::Vector2f size = {200, 50};
-        return createMenuButton(name, onClick, posRatios, size);
-    }
-
-    static Button* createMenuButton(const std::string& name, const std::function<void()>& onClick, sf::Vector2f posRatios, sf::Vector2f size) {
-        auto element = new Button(size, posRatios, onClick);
-
-        auto& rect = element->body;
-        rect.setFillColor(ColorBlue);
-        rect.setOutlineColor(sf::Color::White);
-        rect.setOutlineThickness(4);
-
-        auto text = sf::Text();
-        text.setFont(FontManager::Fonts["jaro"]);
-        text.setCharacterSize(GameInterface::smallCharacterSize);
-        text.setFillColor(sf::Color::White);
-        text.setOutlineColor(sf::Color::Black);
-        text.setOutlineThickness(1);
-        text.setString(name);
-        text.setPosition(rect.getPosition());
-        text.move(rect.getSize().x/2 - text.getGlobalBounds().width/2, rect.getSize().y/2 - text.getGlobalBounds().height + text.getGlobalBounds().height/3);
-        element->setText(text);
-
-        return element;
-    }
-
-    static Panel* createPanel(UIElement* parent, sf::Vector2f size, sf::Vector2f posRatios, PanelType panelType) {
-        auto panel = new Panel(parent, size, posRatios);
-
-        panel->setPanelType(panelType);
-
-        auto& rect = panel->body;
-        rect.setFillColor(sf::Color::Transparent);
-        rect.setOutlineColor(sf::Color::Green);
-        rect.setOutlineThickness(3);
-
-        return panel;
-    }
+    static Panel* createPanel(UIElement* parent, sf::Vector2f size, sf::Vector2f posRatios, PanelType panelType);
 };
