d18d4463a8bf7b5ba8efee27fdd60bd54ea9119c
Tofame <niewiadoma.65@gmail.com>
2024-06-08 22:21:55 +0200

    Add commentary to WordSpawner

--- Game/WordSpawner.cpp
+++ Game/WordSpawner.cpp
@@ -33,6 +33,9 @@ WordSpawner* WordSpawner::getInstance() {
     return wordSpawner_;
 }
 
+// If canSpawnWord() is true then it creates
+// new word at X: left side of panel, Y: randomly chosen
+// The text of Word is set in Word's constructor
 void WordSpawner::spawnWord() {
     if(Game::getInstance()->getGameState() != STATE_PLAYING) {
         return;
@@ -45,6 +48,7 @@ void WordSpawner::spawnWord() {
     auto wordsPanel = GameInterface::getInstance()->getPanelByType(PANEL_WORDS);
     auto word = new Word();
     // Words are not "left upper corner", but they are always -width, so its their position is actually (rightCorner, upper)
+    // ^ The reason words are like that is to they instantly disappear when they touch the right side.
     // Thats why we calculate x ratio here to make them properly aligned to left side of panel
     word->posRatio.setValues(word->getText().getLocalBounds().width / wordsPanel->body.getLocalBounds().width, chooseWordYRatio());
 
@@ -69,6 +73,8 @@ void WordSpawner::spawnWord(float ratioX, float ratioY, std::string text) {
     this->lastSpawnTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
 }
 
+// Checks if enough time has passed (comparess timeSinceLastSpawn with getWordsFrequency())
+// Returns true if word can be spawned
 bool WordSpawner::canSpawnWord() {
     auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
     auto timeSinceLastSpawn = currentTime - lastSpawnTime;
@@ -76,14 +82,14 @@ bool WordSpawner::canSpawnWord() {
     return timeSinceLastSpawn >= std::chrono::milliseconds(static_cast<int>(Settings::getInstance()->getWordsFrequency(false) * 1000));
 }
 
+// Moves all words by getWordsSpeed(false) value.
+// If words are touching the right side (their pos X ratio is at 1.0) then they are deleted.
+// In case of word removal statistics are updated (words missed are increased)
 void WordSpawner::moveWords() {
     auto wordsPanel = GameInterface::getInstance()->getPanelByType(PANEL_WORDS);
     if(wordsPanel == nullptr) {
         return;
     }
-
-    // Updates dynamic label
-    GameStatistics::getInstance()->updateTimePassedSinceStart();
 
     for(auto word : wordsPanel->UIElements) {
         if(word->posRatio.getX() >= 1.0) {
@@ -98,6 +104,11 @@ void WordSpawner::moveWords() {
     }
 }
 
+// Updates statistic - time passed since start
+// Takes input from text field in PANEL_GAMESTATISTICS and compares it with all words
+// to set their highlight width.
+// In case an input found a word that matches it 1:1, then the input is cleared, the word is removed and
+// the statistics are updated (like score and words scored)
 void WordSpawner::manageWords() {
     auto panelGameStatistics = GameInterface::getInstance()->getPanelByType(PANEL_GAMESTATISTICS);
     if(panelGameStatistics == nullptr) {
@@ -120,6 +131,9 @@ void WordSpawner::manageWords() {
     if(panelWords == nullptr) {
         throw std::runtime_error("WordSpawner::manageWords() can't seem to find PANEL_WORDS. Does it exist? (Check GameInterface::getInstance()->setupPanels())");
     }
+
+    // Updates dynamic label containing info about Time that has passed since start of the game.
+    GameStatistics::getInstance()->updateTimePassedSinceStart();
 
     // sf::String converted to utf32 string as we later in code below use e.g.: sf::Uint32 letter = wordString[index];
     auto input = textField->getInputString().toUtf32();
@@ -246,6 +260,7 @@ double chooseWordYRatio() {
     return randomFloat;
 }
 
+// Sets highlight of all bodies to 0 (meaning highlight won't be visible)
 void setAllWordsBodiesToZero(Panel* panel) {
     for(auto word : panel->UIElements) {
         word->body.setSize({0, word->body.getSize().y});
